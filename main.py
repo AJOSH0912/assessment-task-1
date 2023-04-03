d754118ad68f18911bc14dcd563570a84fa78f9a
AJOSH0912 <125627536+AJOSH0912@users.noreply.github.com>
2023-04-04 09:58:59 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import random
 
 briefcases = {1:1, 2:5, 3:10, 4:20, 5:50, 6:100, 7:200, 8:300, 9:1000, 10:1500, 11:2000, 12:3000, 13:5000, 14:7000, 15:10000, 16:20000, 17:30000, 18:50000, 19:70000, 20:100000, 21:150000, 22:200000, 23:300000, 24:500000, 25:750000, 26:1000000}
 remaining_briefcases = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-yes_or_no = input(print("Welcome new player! Would you like to play a game of deal no deal?"))
+yes_or_no = input(print("Welcome new player! Would you like to play a game of deal no deal? Say yes to play, or no to not play"))
 
-if yes_or_no.lower() == "yes":
-  print("Let's start!")
+if yes_or_no == "yes":
+    print("Let's start!")
+elif yes_or_no == "Yes":
+    print("Let's start")
 else:
-  print("How sad!")
+    print("How sad!")
+
+while yes_or_no == "yes":
