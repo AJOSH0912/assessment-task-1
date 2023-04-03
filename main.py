279211a4348a651a38ef2f86f304b06c813cdd32
Aahan-dev <129125344+Aahan-dev@users.noreply.github.com>
2023-04-04 09:51:48 +1000

    commit

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import random
 
-briefcases = {1:1, 2:5, 3:10, 4:20, 5:50, 6:100, 7:200, 8:300, 9:1000, 10:1500, 11:2000, 12:3000, 13:5000, 14:7000, 15:10000, 16:20000, 17:30000, 18:50000, 19:70000, 20:100000, 21:150000, 22:200000, 23:300000, 24:500000, 25:750000, 26:1000000}
-remaining_briefcases = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-yes_or_no = input(print("Welcome new player! Would you like to play a game of deal no deal?"))
-
-if yes_or_no == "yes":
-    print("Let's start!")
-elif yes_or_no == "Yes":
-    print("Let's start")
-else:
-    print("How sad!")
-
- test1
+print("Welcome new player! Would you like to play a game of deal no deal?")
+print()
+print("The rules are simple! There are 26 briefcases you must eliminate 6 briefcases that will contain a random amount of money. Then the bank will offer you a sum of money.")
+print("You may choose to accept or decline the deal, if you accept then you will get the amount of money offered by the bank and the game will end.
+ 
 
