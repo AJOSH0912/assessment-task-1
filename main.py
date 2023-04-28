412d0a5879ea54d3df6136187600f44b21481ba1
Aahan-dev <129125344+Aahan-dev@users.noreply.github.com>
2023-04-28 14:39:21 +1000

    23
    ew

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import random
 from tkinter import * 
 import time
-
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+from tkinter import*
 briefcases = {1:1, 2:5, 3:10, 4:20, 5:50, 6:100, 7:200, 8:300, 9:1000, 10:1500, 11:2000, 12:3000, 13:5000, 14:7000, 15:10000, 16:20000, 17:30000, 18:50000, 19:70000, 20:100000, 21:150000, 22:200000, 23:300000, 24:500000, 25:750000, 26:1000000}
 remaining_briefcases = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+random.shuffle(briefcases)
+print(briefcases)
 
-def offer():
-  val = 0
-  for x in briefcases:
-    val = val+briefcases[x]
-  return val/len(briefcases)  
-
-def shuffle():
   keys = list(briefcases.keys())
   random.shuffle(keys)
   shuffled_briefcases = dict()
@@ -19,27 +17,61 @@ def shuffle():
     shuffled_briefcases.update({i: briefcases[keys]})
   return shuffled_briefcases
 
-
-shuffle
-
 yes_or_no = input("Welcome new player! Would you like to play a game of deal no deal?\n ")
 
 if yes_or_no.lower() == "yes":
-  print(''' Great! Now that you want to play lets lern the rules! There are 26 briefcases ranging from
+  root = Tk()
+  root.geometry("500x300")
+  def getvals():
+    print("Accepted")
+  Label(root, text="Login Credentials", font ="arial 15 bold").grid(row=0, column=3)
+  name= Label(root, text="Name")
+  password= Label(root, text="Password")
+  name.grid(row=1, column= 2)
+  password.grid(row=2, column= 2)
+
+  namevalue = StringVar
+  passwordvalue = StringVar
+  checkvalue = IntVar
+
+  nameentry = Entry(root, textvariable=namevalue)
+  #passwordentry = Entry(root, textvariable=passwordvalue)
+  passwordentry = Entry(root, show="*")
+
+  nameentry.grid(row=1, column=3)
+  passwordentry.grid(row=2, column=3)
+
+  checkbtn = Checkbutton(text="remember me?", variable= checkvalue)
+  checkbtn.grid(row=6, column=3)
+
+  #Button(text="Submit", command=getvals).grid(row=7, column=3 )
+  Button(text="Submit", command=getvals).grid(row=7, column=3 )
+
+else: print("That's sad!")
+  
+
+root.mainloop()
+#exit()
+  
+print(''' Great! Now that you want to play lets lern the rules! There are 26 briefcases ranging from
   values from one dollar to one million dollars.You must now choose to keep one briefcase safe from elimination 
-  this is known as your personal briefcase.You will eliminate 6 briefcases of your choice.
+  this is known as your personal briefcase.It can only be opened if you make it to the end of the game.
+  You will eliminate 6 briefcases of your choice.
   After this the bank will make an offer, it is your choice to accept the offer or not. If you
   take the deal then you will walk away with that amount of money and the game will end. If you do not
   take the offer then you will choose another six briefcases to eliminate. Then the banker will make
   another offer this will continue until either the contestant chooses a deal or they reach the last 
   two remaining briefcases. If the contestant reaches the last two remaining briefcases then the contestant
   must select one and the contestant will get the amount of money in the leftover briefcase.''')
-else:
-  print("That's sad!")
-  exit()
 
+print()
+player_briefcase = int(input("Pick your personal case from the numbers between 1 and 26!\n "))
+  #else:
+  #print("That's sad!")
+  #root.mainloop()
+  #exit()
 while yes_or_no.lower() == "yes":
-  player_briefcase = int(input("Pick your personal case from the numbers between 1 and 26!\n "))
+  
   # Below line is to remove the selected item from the list
   remaining_briefcases.pop(remaining_briefcases.index(int(player_briefcase)))
   while len(remaining_briefcases) > 20:  
@@ -48,6 +80,7 @@ while yes_or_no.lower() == "yes":
       print("Sorry, you can only choose from briefcases going from 1 to 26")
     else:
       print("You eliminated " + str(briefcases[int(user_choice1)]) + " Dollar/s")
+
       remaining_briefcases.pop(remaining_briefcases.index(int(user_choice1)))
 
   print("The banker is thinking of a deal")
@@ -58,13 +91,13 @@ while yes_or_no.lower() == "yes":
   print(".")
   time.sleep(1.2)
 
-  print("The banker offers you $" + str(offer())) 
+  value1 = random.randrange(1, 1000000)
+  print("The banker offers you $" + str(value1))
   time.sleep(2)
   deal_or_no = input("Do you accept this offer(deal) or do you reject this offer(no deal) ")
   print(deal_or_no)
   if deal_or_no == "deal":
-    print("You have won " + str(offer()))
-    exit()
+    print("You have won $ " + str(value1))
   else:
     print("Ok, let's continue!")
   while len(remaining_briefcases) > 14:
@@ -83,13 +116,13 @@ while yes_or_no.lower() == "yes":
   print(".")
   time.sleep(1.2)
 
-  print("The banker offers you $" + str(offer()))
+  value2 = random.randrange(1, 1000000)
+  print("The banker offers you $" + str(value2))
   time.sleep(2)
   deal_or_no1 = input("Do you accept this offer(deal) or do you reject this offer(no deal) ")
   print(deal_or_no1)
   if deal_or_no1 == "deal":
-    print("You have won " + str(offer()))
-    exit()
+    print("You have won $ " + str(value2))
   else:
     print("Ok, let's continue!")
   while len(remaining_briefcases) > 8:
@@ -107,12 +140,13 @@ while yes_or_no.lower() == "yes":
   print(".")
   time.sleep(1.2)
 
-  print("The banker offers you $" + str(offer()))
+  value3 = random.randrange(1, 1000000) 
+  print("The banker offers you $" + str(value3))
   time.sleep(2)
   deal_or_no2 = input("Do you accept this offer(deal) or do you reject this offer(no deal) ")
   print(deal_or_no2)
   if deal_or_no2 == "deal":
-    print("You have won " + str(offer()))
+    print("You have won $" + str(value3))
     exit()
   else:
     print("Ok, let's continue!")
@@ -131,12 +165,13 @@ while yes_or_no.lower() == "yes":
   print(".")
   time.sleep(1.2)
 
-  print("The banker offers you $" + str(offer()))
+  value4 = random.randrange(1, 1000000)
+  print("The banker offers you $" + str(value4))
   time.sleep(2)
   deal_or_no3 = input("Do you accept this offer(deal) or do you reject this offer(no deal) ")
   print(deal_or_no3)
   if deal_or_no3 == "deal":
-    print("You have won " + str(offer()))
+    print("You have won $" + str(value4))
     exit()
   else:
     print("Ok, let's continue!")
@@ -155,12 +190,13 @@ while yes_or_no.lower() == "yes":
   print(".")
   time.sleep(1.2)
   
-  print("The banker offers you $" + str(offer()))
+  value5 = random.randrange(1, 1000000)
+  print("The banker offers you $" + str(value5))
   time.sleep(2)
   deal_or_no4 = input("Do you accept this offer(deal) or do you reject this offer(no deal) ")
   print(deal_or_no4)
   if deal_or_no4 == "deal":
-    print("You have won " + str(offer()))
+    print("You have won $" + str(value5))
     exit()
   else:
     print("Ok, let's continue!")
