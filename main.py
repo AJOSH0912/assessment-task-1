e06f7cd3680dd44425977d68aa4ad1890ddc1998
Aahan-dev <129125344+Aahan-dev@users.noreply.github.com>
2023-04-28 14:49:26 +1000

    er
    ere

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ colorama.init()
 from tkinter import*
 briefcases = {1:1, 2:5, 3:10, 4:20, 5:50, 6:100, 7:200, 8:300, 9:1000, 10:1500, 11:2000, 12:3000, 13:5000, 14:7000, 15:10000, 16:20000, 17:30000, 18:50000, 19:70000, 20:100000, 21:150000, 22:200000, 23:300000, 24:500000, 25:750000, 26:1000000}
 remaining_briefcases = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+<<<<<<< Updated upstream
 random.shuffle(briefcases)
 print(briefcases)
 
@@ -89,6 +90,19 @@ if yes_or_no.lower() == "yes":
   print("Then the banker will make another offer this will continue until either the contestant chooses a deal or they reach the last two remaining briefcases.")
   time.sleep(4)
   print("If the contestant reaches the last two remaining briefcases then the contestant must select one and the contestant will get the amount of money in the leftover briefcase.")
+=======
+yes_or_no = input("Welcome new player! Would you like to play a game of deal no deal?\n")
+
+if yes_or_no.lower() == "yes":
+  rules = ''' That's great! 
+  Now lets go over the rules. There are 26 briefcases with random amount of money to start then the contestant must eliminate 6
+  after this the bank willl offer the contestant an amount of money. It is the contestants choice 
+  to take the money or not. If they do they they will walk away with that amount of money and the game ends 
+  is they choose to not take the deal then they eliminate another 4 briefcases and the bank makes another offer
+   then the game repeats until there are 4 briefcases left. '''
+  print(rules)
+  # double check the rules for deal no deal
+>>>>>>> Stashed changes
 else:
   print("That's sad!")
   exit()
