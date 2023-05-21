56af6e5bfb4a2da7e70cb28561c016675ecce8c4
AJOSH0912 <aryan.joshi@education.nsw.gov.au>
2023-05-22 08:18:01 +1000

    commit

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-# widgets = GUI elements: buttons, textboxes, labels, images
-# windows = seves as a containerto hold or contain these widgets
-
-window = Tk() # will instansiate an instance of a window
-
-window.mainloop()
